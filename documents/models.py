eb25cd8b01f0cebb785726475cda34b7f05427da
Ronald Lopez <lopez.j.ronald@gmail.com>
2022-06-26 23:55:54 -0700

    configuring bulletin model, list, detail, create, update, and delete

--- documents/models.py
+++ documents/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Document(models.Model):
@@ -9,12 +10,27 @@ class Document(models.Model):
         ("PT Form", "PT Form"),
     ]
     title = models.CharField(max_length=500)
-    description = models.TextField()
-    url = models.URLField()
+    description = models.TextField(blank=True, null=True)
+    type = models.CharField(max_length=50, choices=DOCUMENT_TYPES, blank=True, null=True)
+    url = models.URLField(blank=True, null=True)
+    embedded_url = models.URLField(blank=True, null=True)
     document_created = models.DateTimeField(auto_now_add=True)
     document_updated = models.DateTimeField(auto_now=True)
-    type = models.CharField(max_length=50, choices=DOCUMENT_TYPES, blank=True, null=True)
-    embedded_url = models.URLField(blank=True, null=True)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("document_detail", kwargs={"pk": self.pk})
+
+
+class Bulletin(models.Model):
+    start_date = models.DateField()
+    end_date = models.DateField()
+    url = models.URLField()
+
+    class Meta:
+        ordering = ("-start_date",)
+
+    def __str__(self):
+        return f"UTA: {self.start_date.month}"
