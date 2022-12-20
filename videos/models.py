64de6996f3b8e7e0e241a666d4a4705ead39d188
Ronald Lopez <lopez.j.ronald@gmail.com>
2022-12-19 19:28:04 -0800

    added additional video types and allow null/blank

--- videos/models.py
+++ videos/models.py
@@ -7,10 +7,12 @@ class Video(models.Model):
     VIDEO_TYPES = [
         ("Training", "Training"),
         ("PT Assessment", "PT Assessment"),
+        ("Unit", "Unit"),
+        ("Other", "Other"),
     ]
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    type = models.CharField(max_length=50, choices=VIDEO_TYPES)
+    type = models.CharField(max_length=50, blank=True, null=True, choices=VIDEO_TYPES)
     embedded_url = models.CharField(max_length=500)
 
     def __str__(self):
